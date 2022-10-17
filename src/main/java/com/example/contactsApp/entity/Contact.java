567c8ce9a45614133c9b0e429ffefba76cf3c9a1
rolandipkhakadze <rolandi.pkhakadze@gmail.com>
2022-10-17 16:32:51 +0400

    add entities

--- src/main/java/com/example/contactsApp/entity/Contact.java
+++ src/main/java/com/example/contactsApp/entity/Contact.java
@@ -1,4 +1,32 @@
 package com.example.contactsApp.entity;
 
+import lombok.*;
+
+import javax.persistence.*;
+
+@Getter
+@Setter
+@ToString
+@NoArgsConstructor
+@AllArgsConstructor
+@EqualsAndHashCode
+@Entity
+@Table
 public class Contact {
+    @Id
+    @GeneratedValue(strategy = GenerationType.SEQUENCE, generator = "contact_id_gen")
+    @SequenceGenerator(name = "contact_id_gen", sequenceName = "contact_id_seq")
+    private Long id;
+    @OneToOne
+    @JoinColumn(name = "phone_number_id")
+    private PhoneNumber phoneNumber;
+    @Column(name = "is_favorite")
+    private boolean isFavorite;
+    @ManyToOne
+    @JoinColumn(name = "user_id")
+    private User user;
+    @Column(name = "first_name")
+    private String firstName;
+    @Column(name = "last_name")
+    private String lastName;
 }
