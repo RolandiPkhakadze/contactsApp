607c1d335718ec096dcbef7351b4b4ca9605dabe
gigitsirekidze <g_tsirekidze@cu.edu.ge>
2022-10-17 16:26:33 +0400

    "Repositories"

---
+++ src/main/java/com/example/contactsApp/repository/UserRepository.java
@@ -0,0 +1,9 @@
+package com.example.contactsApp.repository;
+
+import com.example.contactsApp.entity.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface UserRepository  extends JpaRepository<User, Long> {
+}
